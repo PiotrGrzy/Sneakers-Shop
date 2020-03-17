becfd72c0b0fcf85a98d9a15affe94cce7518b92
unknown <piotr.grzymowicz1@gmail.com>
2020-03-17 16:23:01 +0100

    add state persist

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore } from 'redux-persist';
 import thunk from 'redux-thunk';
 import reducers from './redux/reducers';
 import App from './components/App';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,13 +1,23 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import shopReducer from './shop/shop.reducer';
 import cartReducer from './cart/cart.reducer';
 import searchReducer from './search/search.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart', 'user']
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   shop: shopReducer,
   cart: cartReducer,
   search: searchReducer
 });
+
+export default persistReducer(persistConfig, rootReducer);
